825de6a5dff1904adf6f7d05db924db54d77f7ba
NetherCod <six0860613@gmail.com>
2021-09-07 11:00:52 +0800

    0907-02

--- index.js
+++ index.js
@@ -9,13 +9,24 @@ app.set('view engine', 'ejs');
 
 //設定靜態內容的資料夾
 app.use('/',express.static(__dirname + '/public'));
-// app.use('/jquery', express.static('node_modules/jquery/dist'));
+// app.use('/jquery', express.static('node_modules/jquery/dist')); 可以引用node_modules
 // app.use('/bootstrap', express.static('node_modules/bootstrap/dist'));
 
 //設定路由 start
 app.get('/', (_req, _res)=>{
     // _res.send(`n<h2>Hello</h2>`); //send內容會自動視為html
     _res.render('home', {name: 'NetherCod'});
+});
+
+app.get('/json-sales', (_req, _res)=>{
+    const sales = require('./data/sales.json');
+    // console.log(sales);
+    // _res.json(sales);
+    _res.render('json-sales',{sales});
+});
+
+app.get('/try-qs', (_req, _res)=>{
+    _res.json(_req.query); //把網址中?後的get資料傳遞包成query物件
 });
 
 //設定路由 end
