94848cdb52db45e5fba64226baa4f133834592a8
NetherCod <six0860613@gmail.com>
2021-09-07 10:09:26 +0800

    0907-01

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ require('dotenv').config(); //使用dotenv的套件去載入.env檔案
 const express = require('express');
 const app = express();
 
+//設定樣板
+app.set('view engine', 'ejs');
+
 //設定靜態內容的資料夾
-app.use(express.static(__dirname + '/public')); 
+app.use('/',express.static(__dirname + '/public'));
+// app.use('/jquery', express.static('node_modules/jquery/dist'));
+// app.use('/bootstrap', express.static('node_modules/bootstrap/dist'));
 
 //設定路由 start
 app.get('/', (_req, _res)=>{
-    _res.send(`<h2>Hello</h2>`); //send內容會自動視為html
+    // _res.send(`n<h2>Hello</h2>`); //send內容會自動視為html
+    _res.render('home', {name: 'NetherCod'});
 });
 
 //設定路由 end
